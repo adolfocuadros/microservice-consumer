a55bd8a3c69624941ebb48dea00f0d4f88114852
Adolfo Cuadros <ronnie.adolfo@gmail.com>
2016-10-31 10:33:16 -0500

    Request en client

--- src/Client.php
+++ src/Client.php
@@ -9,18 +9,19 @@ class Client extends ClientBase
 {
     function __construct($url, $token = null)
     {
+        $request = app('request');
         $config = [
             'base_uri'=> $url,
-            'timeout' => 5.0
+            'timeout' => 20.0
         ];
 
         if($token != null) {
             $config['headers'] = [
                 'Auth-Token' => $token
             ];
-        } elseif(!empty(request()->header('Auth-Token'))) {
+        } elseif(!empty($request->header('Auth-Token'))) {
             $config['headers'] = [
-                'Auth-Token' => request()->header('Auth-Token')
+                'Auth-Token' => $request->header('Auth-Token')
             ];
         }
 
